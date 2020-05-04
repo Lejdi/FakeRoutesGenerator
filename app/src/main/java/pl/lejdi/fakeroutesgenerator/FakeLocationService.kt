dbe3c8b77e36695c128e68baa1b9c6e77dfc0223
Lejdi <lejdi1618@gmail.com>
2020-05-04 18:39:36 +0200

    Create service that runs in background

--- app/src/main/java/pl/lejdi/fakeroutesgenerator/FakeLocationService.kt
+++ app/src/main/java/pl/lejdi/fakeroutesgenerator/FakeLocationService.kt
@@ -1,4 +1,42 @@
 package pl.lejdi.fakeroutesgenerator
 
-class FakeLocationService {
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+
+class FakeLocationService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotification()
+    }
+
+    private fun createNotification(){
+        val notifBuilder = NotificationCompat.Builder(this, "pl.lejdi.fakeroutesgenerator")
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.common_google_signin_btn_text_dark)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .setPriority(NotificationManager.IMPORTANCE_HIGH)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+
+        val chan = NotificationChannel("pl.lejdi.fakeroutesgenerator", "Fake Locations", NotificationManager.IMPORTANCE_HIGH)
+        chan.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+
+        val notifManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notifManager.createNotificationChannel(chan)
+
+        val notification = notifBuilder.build()
+        notifManager.notify(2, notification)
+
+        startForeground(2, notification)
+    }
 }
--- app/src/main/java/pl/lejdi/fakeroutesgenerator/MainActivity.kt
+++ app/src/main/java/pl/lejdi/fakeroutesgenerator/MainActivity.kt
@@ -1,18 +1,55 @@
 package pl.lejdi.fakeroutesgenerator
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
 class MainActivity : AppCompatActivity() {
+    private var serviceStarted = false
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.start_button)
+        setButtonClickListener()
+    }
 
-        getRoute()
+    private fun setButtonClickListener()
+    {
+        button.setOnClickListener {
+            if (!serviceStarted) {
+                serviceStarted = true
+                button.text = "STOP"
+                runService()
+            } else {
+                serviceStarted = false
+                button.text = "START"
+                abortService()
+            }
+        }
+    }
+
+    private fun runService()
+    {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 2000)
+        }
+
+        startForegroundService(Intent(applicationContext, FakeLocationService::class.java))
+    }
+
+    private fun abortService()
+    {
+        stopService(Intent(this, FakeLocationService::class.java))
     }
 
     private fun getRoute()
