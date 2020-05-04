7b67ddbff114c3ae5529e3f31494dd65f92b7047
Lejdi <lejdi1618@gmail.com>
2020-05-04 15:39:37 +0200

    Add model and hardcoded routes

---
+++ app/src/main/java/pl/lejdi/fakeroutesgenerator/FakeLocationService.kt
@@ -0,0 +1,4 @@
+package pl.lejdi.fakeroutesgenerator
+
+class FakeLocationService {
+}
---
+++ app/src/main/java/pl/lejdi/fakeroutesgenerator/Route.kt
@@ -0,0 +1,5 @@
+package pl.lejdi.fakeroutesgenerator
+
+import com.google.android.gms.maps.model.LatLng
+
+class Route (val verts : ArrayList<LatLng>)
