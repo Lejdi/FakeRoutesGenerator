ea147aa5487b49f6539720820ec9c80bac71adce
Lejdi <lejdi1618@gmail.com>
2020-05-04 16:16:49 +0200

    Move hardcoded routes to resources

--- app/src/main/java/pl/lejdi/fakeroutesgenerator/MainActivity.kt
+++ app/src/main/java/pl/lejdi/fakeroutesgenerator/MainActivity.kt
@@ -2,11 +2,22 @@ package pl.lejdi.fakeroutesgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getRoute()
+    }
+
+    private fun getRoute()
+    {
+        val gson = Gson()
+        val route = gson.fromJson(HardcodedRoutes.r3, Route::class.java)
     }
 }
